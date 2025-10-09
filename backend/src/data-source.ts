274aadf620afea85c6be575f4e3bae7b334eede8
Sharon Yang <sharonyyang0@gmail.com>
2025-10-09 15:09:28 -0400

    finish editing data sources

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,16 +2,18 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: DB_HOST,
+  port: parseInt(DB_PORT || "5432"),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   synchronize: true,
-  logging: false,
+  logging: true,
   entities: [User],
-  migrations: [],
+  migrations: ["src/migration/**/*.ts"],
   subscribers: [],
 });
