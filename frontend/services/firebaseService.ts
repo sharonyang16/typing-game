b14606da98a31ee5f13ebeea7fb47c189cb9d18d
Sharon Yang <sharonyyang0@gmail.com>
2025-10-13 14:08:28 -0400

    create firebase service functions

--- frontend/services/firebaseService.ts
+++ frontend/services/firebaseService.ts
@@ -1,4 +1,13 @@
 import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  PasswordValidationStatus,
+  signInWithEmailAndPassword,
+  signOut,
+  UserCredential,
+  validatePassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -11,5 +20,28 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default app;
+const signUp = async (
+  email: string,
+  password: string
+): Promise<UserCredential> => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const logIn = async (
+  email: string,
+  password: string
+): Promise<UserCredential> => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const checkPassword = async (
+  password: string
+): Promise<PasswordValidationStatus> => {
+  return await validatePassword(auth, password);
+};
+
+const logOut = async (): Promise<void> => signOut(auth);
+
+export { auth, checkPassword, signUp, logIn, logOut };
