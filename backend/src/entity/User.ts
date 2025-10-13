73dc00aae3531cb5c604f241c800e614d649282a
Sharon Yang <sharonyyang0@gmail.com>
2025-10-13 12:57:31 -0400

    add comments

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -1,5 +1,15 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
+/**
+ * Entity definition for a single user.
+ * @class User
+ * 
+ * Each User includes the following fields:
+ * - id: The unique id of the user.
+ * - email: The email of the user.
+ * - username: The username of the user.
+ * - dateJoined: The date the user joined.
+ */
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
