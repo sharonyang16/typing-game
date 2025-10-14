2a497087ba9b968bb6837470ff93c1ec28fa44c0
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-10-13 20:03:35 -0400

    Add Header Link + Edit Restart Button (#24)
    * add link to header
    
    * edit restart button

--- frontend/components/layout/header.tsx
+++ frontend/components/layout/header.tsx
@@ -3,7 +3,9 @@ import { User } from "lucide-react";
 const Header = () => {
   return (
     <header className="w-full flex justify-between align-center py-4">
-      <div className="font-mono text-xl"> typing game</div>
+      <div className="font-mono text-xl font-bold">
+        <Link href="/">typing game</Link>
+      </div>
       <Link href="/sign-up">
         <User />
       </Link>
