e74d0dafd60a670946ae388338587439032e0df6
Sharon Yang <sharonyyang0@gmail.com>
2025-11-10 21:51:34 -0500

    add cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { Request, Response } from "express";
+import { cookieParser } from "cookie-parser";
 import cors from "cors";
+
 import userController from "./controllers/user.controller";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const port = parseInt(process.env.PORT || "8000");
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
