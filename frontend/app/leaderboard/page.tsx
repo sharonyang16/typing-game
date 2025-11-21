cf44511d2074f4d44f13c57c3d514c8de56d9408
Sharon Yang <92332953+sharonyang16@users.noreply.github.com>
2025-11-21 10:22:36 -0500

    Update Leaderboard (#66)
    * add wordCount and usedCapitals query options
    
    * working filters
    
    * add onchange + state var
    
    * show caps column only if all/any
    
    * add suspense
    
    * rename const

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -3,9 +3,19 @@
 import useLeaderboardPage from "@/hooks/useLeaderboardPage";
 import { Check, X } from "lucide-react";
 import Link from "next/link";
+import numWordsConstant from "@/static/numWords.json";
+import capitalsConstant from "@/static/capitalsFilterOptions.json";
 
 const LeaderboardPage = () => {
-  const { tests, showSignUpBanner } = useLeaderboardPage();
+  const {
+    tests,
+    showSignUpBanner,
+    handleWordCountChange,
+    handleCapitalsChange,
+    selectedWordCount,
+    selectedCapitals,
+  } = useLeaderboardPage();
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-xl font-bold">Leaderboard</h1>
@@ -28,34 +38,73 @@ const LeaderboardPage = () => {
           </div>
         </div>
       )}
-      <table className="table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>User</th>
-            {/*<th>Words</th>*/}
-            <th>WPM</th>
-            <th>Time</th>
-            <th>Accuracy</th>
-            {/*<th>Caps</th>*/}
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tests.map((test, index) => (
-            <tr key={`test-${index}`}>
-              <td>{index + 1}</td>
-              <td>{test.user}</td>
-              {/*<td>{test.words}</td>*/}
-              <td>{test.wpm}</td>
-              <td>{test.timeToComplete}</td>
-              <td>{test.accuracy}</td>
-              {/*<td>{test.useCapitals ? <Check /> : <X />}</td>*/}
-              <td>{test.date}</td>
+      <div className="flex gap-8 items-start">
+        <div className="flex flex-col gap-4">
+          <fieldset className="fieldset">
+            <label className="label">Word Count</label>
+            <div className="join join-vertical">
+              {numWordsConstant.map((value) => (
+                <input
+                  className="join-item btn"
+                  key={`numWords-${value}`}
+                  type="radio"
+                  name="word-count-options"
+                  value={value}
+                  checked={selectedWordCount === value}
+                  aria-label={value.toString()}
+                  onChange={handleWordCountChange}
+                />
+              ))}
+            </div>
+          </fieldset>
+          <fieldset className="fieldset">
+            <label className="label">Capitals</label>
+            <div className="join join-vertical">
+              {capitalsConstant.map(({ label, value }) => (
+                <input
+                  className="join-item btn"
+                  key={`numWords-${value}`}
+                  type="radio"
+                  name="caps-options"
+                  value={value}
+                  checked={selectedCapitals === value}
+                  aria-label={label}
+                  onChange={handleCapitalsChange}
+                />
+              ))}
+            </div>
+          </fieldset>
+        </div>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>User</th>
+              <th>WPM</th>
+              <th>Time</th>
+              <th>Accuracy</th>
+              {!selectedCapitals && <th>Caps</th>}
+              <th>Date</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tests.map((test, index) => (
+              <tr key={`test-${index}`}>
+                <td>{index + 1}</td>
+                <td>{test.user}</td>
+                <td>{test.wpm}</td>
+                <td>{test.timeToComplete}</td>
+                <td>{test.accuracy}</td>
+                {!selectedCapitals && (
+                  <td>{test.useCapitals ? <Check /> : <X />}</td>
+                )}
+                <td>{test.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- frontend/hooks/useLeaderboardPage.ts
+++ frontend/hooks/useLeaderboardPage.ts
@@ -7,16 +7,29 @@ import {
 } from "@/types/typing-test";
 import { useAuthContext } from "@/context/AuthContext";
 import { format } from "date-fns";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const useLeaderboardPage = () => {
   const [tests, setTests] = useState<TypingTestLeaderboardEntry[]>([]);
   const [showSignUpBanner, setShowSignUpBanner] = useState(false);
-
+  const [selectedWordCount, setSelectedWordCount] = useState(25);
+  const [selectedCapitals, setSelectedCapitals] = useState("");
   const { user } = useAuthContext();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    const wordCount = searchParams.get("wordCount") ?? "25";
+    setSelectedWordCount(parseInt(wordCount));
+    const useCapitals = searchParams.get("usedCapitals");
+
     const getTests = async () => {
-      const tests = await getAllTests("orderByField=wpm");
+      const tests = await getAllTests(
+        `orderByField=wpm&wordCount=${wordCount}${
+          useCapitals ? `&usedCapitals=${useCapitals}` : ""
+        }`
+      );
 
       setTests(
         tests.map((test: PopulatedTypingTest) => {
@@ -30,7 +43,31 @@ const useLeaderboardPage = () => {
       );
     };
     getTests();
-  }, []);
+  }, [searchParams]);
+
+  const handleWordCountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("wordCount", e.target.value || "");
+      setSelectedWordCount(parseInt(e.target.value || "25"));
+
+      router.push(pathname + "?" + params.toString());
+    } catch {
+      // do nothing
+    }
+  };
+
+  const handleCapitalsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("usedCapitals", e.target.value || "");
+      setSelectedCapitals(e.target.value || "");
+
+      router.push(pathname + "?" + params.toString());
+    } catch {
+      // do nothing
+    }
+  };
 
   useEffect(() => {
     setShowSignUpBanner(!user);
@@ -39,6 +76,10 @@ const useLeaderboardPage = () => {
   return {
     tests,
     showSignUpBanner,
+    handleWordCountChange,
+    handleCapitalsChange,
+    selectedWordCount,
+    selectedCapitals,
   };
 };
 
