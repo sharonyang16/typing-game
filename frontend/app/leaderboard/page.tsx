23ed18916db7198793e375ed4f7ea756882de4ea
Sharon Yang <sharonyyang0@gmail.com>
2025-12-10 08:41:15 -0500

    create leaderboard component

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import useLeaderboardPage from "@/hooks/useLeaderboardPage";
-import { Check, X } from "lucide-react";
 import Link from "next/link";
 import numWordsConstant from "@/static/numWords.json";
 import capitalsConstant from "@/static/capitalsFilterOptions.json";
 import Banner from "@/components/banner/banner";
+import Leaderboard from "@/components/leaderboard/table";
 
 const LeaderboardPage = () => {
   const {
@@ -77,59 +77,11 @@ const LeaderboardPage = () => {
           </fieldset>
         </div>
 
-        <table className="table table-fixed">
-          <thead>
-            <tr>
-              <th className="w-12"></th>
-              <th className="w-48">User</th>
-              <th>WPM</th>
-              <th>Time</th>
-              <th>Acc.</th>
-              {!selectedCapitals && <th>Caps</th>}
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loading &&
-              [...Array(5)].map((_, index) => (
-                <tr key={`skeleton-row-${index}`}>
-                  {[...Array(selectedCapitals ? 6 : 7)]
-                    .fill(selectedCapitals ? 6 : 7)
-                    .map((_, index) => (
-                      <td key={`skeleton-${index}`}>
-                        <div className="skeleton h-4" />
-                      </td>
-                    ))}
-                </tr>
-              ))}
-            {!loading && tests.length === 0 ? (
-              <tr>
-                <td
-                  colSpan={selectedCapitals ? 6 : 7}
-                  className="py-8 text-center"
-                >
-                  No tests yet
-                </td>
-              </tr>
-            ) : (
-              tests.map((test, index) => (
-                <tr key={`test-${index}`}>
-                  <td>{index + 1}</td>
-                  <td className="whitespace-nowrap overflow-hidden text-ellipsis">
-                    {test.user}
-                  </td>
-                  <td>{test.wpm}</td>
-                  <td>{test.timeToComplete}</td>
-                  <td>{test.accuracy}</td>
-                  {!selectedCapitals && (
-                    <td>{test.useCapitals ? <Check /> : <X />}</td>
-                  )}
-                  <td>{test.date}</td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+        <Leaderboard
+          tests={tests}
+          loading={loading}
+          selectedCapitals={!!selectedCapitals}
+        />
       </div>
     </div>
   );
---
+++ frontend/components/leaderboard/table.tsx
@@ -0,0 +1,68 @@
+import { TypingTestLeaderboardEntry } from "@/types/typing-test";
+import { Check, X } from "lucide-react";
+
+type LeaderboardProps = {
+  tests: TypingTestLeaderboardEntry[];
+  loading: boolean;
+  selectedCapitals: boolean;
+};
+const Leaderboard = ({
+  tests,
+  loading,
+  selectedCapitals,
+}: LeaderboardProps) => {
+  return (
+    <table className="table table-fixed">
+      <thead>
+        <tr>
+          <th className="w-12"></th>
+          <th className="w-48">User</th>
+          <th>WPM</th>
+          <th>Time</th>
+          <th>Acc.</th>
+          {!selectedCapitals && <th>Caps</th>}
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {loading &&
+          [...Array(5)].map((_, index) => (
+            <tr key={`skeleton-row-${index}`}>
+              {[...Array(selectedCapitals ? 6 : 7)]
+                .fill(selectedCapitals ? 6 : 7)
+                .map((_, index) => (
+                  <td key={`skeleton-${index}`}>
+                    <div className="skeleton h-4" />
+                  </td>
+                ))}
+            </tr>
+          ))}
+        {!loading && tests.length === 0 ? (
+          <tr>
+            <td colSpan={selectedCapitals ? 6 : 7} className="py-8 text-center">
+              No tests yet
+            </td>
+          </tr>
+        ) : (
+          tests.map((test, index) => (
+            <tr key={`test-${index}`}>
+              <td>{index + 1}</td>
+              <td className="whitespace-nowrap overflow-hidden text-ellipsis">
+                {test.user}
+              </td>
+              <td>{test.wpm}</td>
+              <td>{test.timeToComplete}</td>
+              <td>{test.accuracy}</td>
+              {!selectedCapitals && (
+                <td>{test.useCapitals ? <Check /> : <X />}</td>
+              )}
+              <td>{test.date}</td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default Leaderboard;
