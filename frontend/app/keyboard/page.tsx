cc7e7a09e9107a249704a5e3f56b48dffa612431
Sharon Yang <sharonyyang0@gmail.com>
2026-03-24 17:29:28 -0400

    refactor again + add reset button

--- frontend/app/keyboard/page.tsx
+++ frontend/app/keyboard/page.tsx
@@ -3,31 +3,41 @@
 import useKeyboardPage from "@/hooks/useKeyboardPage";
 
 const KeyboardPage = () => {
-  const { keyboard, currentPressedKeys, allPressedKeys } = useKeyboardPage();
+  const { keyboard, currentPressedKeys, allPressedKeys, resetPressedKeys } =
+    useKeyboardPage();
   return (
     <div className="flex flex-col gap-16">
       <h1 className="text-xl font-bold">Key Tester</h1>
+
       <div className="w-full flex justify-center align-center">
-        <div className="flex-col">
-          {keyboard.map((row, index) => (
-            <div
-              className="my-1 flex w-full gap-1"
-              key={`keyboard-row-${index}`}
-            >
-              {row.map((key, index) => (
-                <kbd
-                  className={`kbd ${currentPressedKeys.has(key.value) ? "bg-accent/60" : allPressedKeys.has(key.value) && "bg-accent text-accent-content"}`}
-                  style={{
-                    minWidth: key.width * 48,
-                    minHeight: 48,
-                  }}
-                  key={key.value + index}
-                >
-                  {key.label}
-                </kbd>
-              ))}
-            </div>
-          ))}
+        <div className="flex flex-col gap-4">
+          <button
+            className="btn btn-primary w-fit self-end"
+            onClick={resetPressedKeys}
+          >
+            Reset
+          </button>
+          <div className="flex-col">
+            {keyboard.map((row, index) => (
+              <div
+                className="my-1 flex w-full gap-1"
+                key={`keyboard-row-${index}`}
+              >
+                {row.map((key, index) => (
+                  <kbd
+                    className={`kbd ${currentPressedKeys.has(key.value) ? "bg-accent/60" : allPressedKeys.has(key.value) && "bg-accent text-accent-content"}`}
+                    style={{
+                      minWidth: key.width * 48,
+                      minHeight: 48,
+                    }}
+                    key={key.value + index}
+                  >
+                    {key.label}
+                  </kbd>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- frontend/hooks/useKeyboardPage.ts
+++ frontend/hooks/useKeyboardPage.ts
@@ -1,9 +1,12 @@
-import useTestKeyboardEvents from "./useTestKeyboardEvents";
 import { Key } from "@/types/keyboard";
 import { KEY_VALUES, KEY_LABELS, KEY_WIDTHS } from "@/static/keyboardConstants";
+import { useEffect, useState } from "react";
 
 const useKeyboardPage = () => {
-  const { currentPressedKeys, allPressedKeys } = useTestKeyboardEvents();
+  const [currentPressedKeys, setCurrentPressedKeys] = useState<Set<string>>(
+    new Set(),
+  );
+  const [allPressedKeys, setAllPressedKeys] = useState<Set<string>>(new Set());
   const keyboard: Key[][] = KEY_VALUES.map((row) =>
     row.map((value) => ({
       value,
@@ -12,7 +15,39 @@ const useKeyboardPage = () => {
     })),
   );
 
-  return { keyboard, currentPressedKeys, allPressedKeys };
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.repeat) return; // ignore key held auto-repeat events
+      setCurrentPressedKeys((prev) => new Set(prev).add(e.key));
+      setAllPressedKeys((prev) => new Set(prev).add(e.key));
+    };
+
+    const handleKeyUp = (e: KeyboardEvent): void => {
+      setCurrentPressedKeys((prev) => {
+        const next = new Set(prev);
+        next.delete(e.key);
+        return next;
+      });
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  });
+
+  const resetPressedKeys = () => {
+    setCurrentPressedKeys(new Set());
+    setAllPressedKeys(new Set());
+  };
+
+  return { keyboard, currentPressedKeys, allPressedKeys, resetPressedKeys };
 };
 
 export default useKeyboardPage;
